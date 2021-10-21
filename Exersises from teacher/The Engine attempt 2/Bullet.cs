d330e470aa3753dbbf20a23f5b089cb4ef20cbd2
jasPerSh122902 <88592760+jasPerSh122902@users.noreply.github.com>
2021-10-21 13:20:14 -0500

    Add files via upload

---
+++ Exersises from teacher/The Engine attempt 2/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibaray;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Bullet : Actor
+    {
+        private float _speed;
+        private Icon _icon;
+        private string _name;
+        public Player _player;
+
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public float Speed
+        {
+            get { return _speed; }
+        }
+
+        public Icon Icon
+        {
+            get { return _icon; }
+        }
+        public Bullet(char Icon, Player player, Color color, float speed, string name = "Bullet")
+        {
+            _player = player;
+            _speed = speed;
+
+
+        }
+    }
+}
